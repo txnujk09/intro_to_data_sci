e5b485d62b0c0779a21ae4ddf04ade0f11049e00
rr1725ic <ruhan.dagar3103@gmail.com>
2025-12-10 17:09:50 +0000

    Update Asia neet.R

--- SDG8_Project/analysis/Asia neet.R
+++ SDG8_Project/analysis/Asia neet.R
@@ -2,10 +2,11 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
-# Load the merged dataset (NEET + continents + HDI)
+#Load dataset containing NEET values merged with continent labels and HDI
 df <- readr::read_csv("youth_continents_hdi_2000_2020.csv")
 
 asia <- df %>%
+#subset Asia and compute yearly average NEET for the continent
   filter(Continent == "Asia") %>%
   group_by(Year) %>%
   summarise(
@@ -13,6 +14,7 @@ asia <- df %>%
     .groups = "drop"
   )
 
+# Plot Asia's average NEET trend over 2000–2020
 ggplot(asia, aes(x = Year, y = mean_neet)) +
   geom_line(linewidth = 1) +
   geom_point(size = 1.3) +
