d7d6584d015a7e643fe4cf40b750f5ed587e4522
rr1725ic <ruhan.dagar3103@gmail.com>
2025-12-10 17:12:15 +0000

    Update Europe neet.R

--- SDG8_Project/analysis/Europe neet.R
+++ SDG8_Project/analysis/Europe neet.R
@@ -2,10 +2,11 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
-# Load the merged dataset (NEET + continents + HDI)
+#Load dataset containing NEET values merged with continent labels and HDI
 df <- readr::read_csv("youth_continents_hdi_2000_2020.csv")
 
 europe <- df %>%
+#subset Europe and compute yearly average NEET for the continent
   filter(Continent == "Europe") %>%
   group_by(Year) %>%
   summarise(
@@ -13,6 +14,7 @@ europe <- df %>%
     .groups = "drop"
   )
 
+# Plot Europe's average NEET trend over 2000–2020
 ggplot(europe, aes(x = Year, y = mean_neet)) +
   geom_line(linewidth = 1) +
   geom_point(size = 1.3) +
