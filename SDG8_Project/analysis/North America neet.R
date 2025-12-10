595eca8b628548737b647fa4a14e1b8b30e16eb8
rr1725ic <ruhan.dagar3103@gmail.com>
2025-12-10 17:13:24 +0000

    Update North America neet.R

--- SDG8_Project/analysis/North America neet.R
+++ SDG8_Project/analysis/North America neet.R
@@ -2,10 +2,11 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
-# Load the merged dataset (NEET + continents + HDI)
+# Load dataset containing NEET values merged with continent labels and HDI
 df <- readr::read_csv("youth_continents_hdi_2000_2020.csv")
 
 north_america <- df %>%
+# subset North America and compute yearly average NEET for the continent
   filter(Continent == "North America") %>%
   group_by(Year) %>%
   summarise(
@@ -13,6 +14,7 @@ north_america <- df %>%
     .groups = "drop"
   )
 
+# Plot North America's average NEET trend over 2000–2020
 ggplot(north_america, aes(x = Year, y = mean_neet)) +
   geom_line(linewidth = 1) +
   geom_point(size = 1.3) +
